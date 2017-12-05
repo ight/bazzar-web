b7f6fdb29a85763b4643007be9010c40746ef350
prakash bhakat <prakash.bhakat2@tcs.com>
2017-12-05 21:12:24 +0530

    Bazzar: Added component for navigation,side&center
      * Added components and some html to figure out
        how it works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { AppComponent } from './app.component';
-
-import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CenterComponent } from './center/center.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightSidebarComponent,
+    NavbarComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule,
